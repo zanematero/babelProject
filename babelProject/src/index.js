d097110986c59f16b2b2c45dc31e99dc5881a8b2
Zane Matero <materozane@gmail.com>
2023-03-06 12:32:05 -0800

    initial

---
+++ babelProject/src/index.js
@@ -0,0 +1,7 @@
+const multiply = (a, b) => a * b
+
+const firstNum = 2 
+const secondNum = 3
+
+let product = multiply(firstNum, secondNum)
+console.log(`The product of ${firstNum} and ${secondNum} is ${product}.`)
